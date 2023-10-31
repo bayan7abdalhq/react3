ad86062a483ad3bc2188b5d304f4de177ef47763
bayan7abdalhq <s12114374@stu.najah.edu>
2023-11-01 00:21:04 +0200

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import Products from './components/product/Products.jsx';
 import Navbar from './components/navbar/Navbar.jsx';
-import { Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Resturant from './components/resturant/Resturant.jsx'; 
 import ResturantDetails from './components/resturant/ResturantDetails.jsx'; 
 
 export default function App() {
   return (
     <>
+    <BrowserRouter>
      <Navbar />
      <Routes>
       <Route path='/products' element={<Products/>} />
       <Route path='/resturant' element={<Resturant/>} />
       <Route path='/resturant/:id' element={<ResturantDetails/>} />
      </Routes>
+     </BrowserRouter>
     </>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,29 +1,25 @@
-import React from 'react'
 
-export default function Navbar() {
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+function Navbarr() {
   return (
-   <>
-<nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link" href="products">Products</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="resturant">Resturant</a>
-        </li>
-      </ul>
-     
-    </div>
-  </div>
-</nav>
-
-
-   </>
-  )
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+           
+            <Link to="products" className='me-3'>Products</Link>
+            <Link to="resturant">Resturant</Link>
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
+export default Navbarr;
